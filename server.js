ab15b2c672280ecf70c1cd47df6834b5ebc8d97c
avinashnk613 <30667723+avinashnk613@users.noreply.github.com>
2017-08-05 09:12:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req, res) {
-   res.send('Article one has been requested'); 
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function(req, res) {
-    res.send('Article two has been requested');
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three', function(req, res) {
-    res.send('Article three has been requested');
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
