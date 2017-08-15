996a446b7300b68ee5f34964c6b453ce29e970d4
avinashnk613 <30667723+avinashnk613@users.noreply.github.com>
2017-08-16 00:22:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -99,7 +99,7 @@ submit.onclick = function() {
     console.log(password);
     request.open('POST', 'http://avinashnk613.imad.hasura-app.io/login', true);
     request.setRequestHeader('Content-Type', 'applicaton/json');
-    request.send(JSON.strungify({username : username, password : password}));
+    request.send(JSON.stringify({username : username, password : password}));
 };
 
 
