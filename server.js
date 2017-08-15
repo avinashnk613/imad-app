30d63780e6980ec00fc5a799066098acb8a0164e
avinashnk613 <30667723+avinashnk613@users.noreply.github.com>
2017-08-15 21:23:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.get('/', function (req, res) {
 
 function hash(input, salt) {
     var hashed = crypto.pbkdf2Sync(input, salt, 100000, 512, 'sha512');
-    return [pkkdf2, 10000, salt, hashed.toString('hex')].join('$');
+    return ["pkkdf2", "10000", salt, hashed.toString('hex')].join('$');
 }
 
 
